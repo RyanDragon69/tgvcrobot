8371e48443e7b251b7c37a3ef4af17c056f9b8ce
NekkoYato <89446086+NekkoYato@users.noreply.github.com>
2021-10-30 13:07:15 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,12 +2,12 @@ echo "Cloning Repo...."
 if [ -z $BRANCH ]
 then
   echo "Cloning main branch...."
-  git clone https://github.com/subinps/VCPlayerBot /VCPlayerBot
+  git clone https://github.com/NekkoYato/tgvcbot /tgvcbot
 else
   echo "Cloning $BRANCH branch...."
-  git clone https://github.com/subinps/VCPlayerBot -b $BRANCH /VCPlayerBot
+  git clone https://github.com/NekkoYato/tgvcbot -b $BRANCH /tgvcbot
 fi
-cd /VCPlayerBot
+cd /tgvcbot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
